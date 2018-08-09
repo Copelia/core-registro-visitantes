f4c95b68c6b2a6d6dc180a1a7ce564d129feb32c
[Copelia] <iamcorallium@gmail.com>
2018-08-09 18:21:50 -0500

    Validando cámara con servidor y en local, también db funciona. Se corrige estilo de botón enviar

--- src/js/main.js
+++ src/js/main.js
@@ -20,8 +20,7 @@ let db = firebase.firestore();
     });
   });
 
-
-//Tomando foto
+//Tomando foto    
 
 (function() {
 
@@ -32,12 +31,12 @@ let db = firebase.firestore();
       startbutton  = document.querySelector('#startbutton'),
       width = 320,
       height = 0;
-
+ 
   navigator.getMedia = ( navigator.getUserMedia ||
                          navigator.webkitGetUserMedia ||
                          navigator.mozGetUserMedia ||
                          navigator.msGetUserMedia);
-
+ 
   navigator.getMedia(
     {
       video: true,
@@ -56,7 +55,7 @@ let db = firebase.firestore();
       console.log("An error occured! " + err);
     }
   );
-
+ 
   video.addEventListener('canplay', function(ev){
     if (!streaming) {
       height = video.videoHeight / (video.videoWidth/width);
@@ -67,7 +66,7 @@ let db = firebase.firestore();
       streaming = true;
     }
   }, false);
-
+ 
   const takepicture = () => {
     canvas.width = width;
     canvas.height = height;
@@ -76,56 +75,23 @@ let db = firebase.firestore();
     //photo.setAttribute('src', data);
     //console.log(data);
   }
-
+ 
   startbutton.addEventListener('click', function(event){
       takepicture();
       getBase64();
     event.preventDefault();
   }, false);
-
-})();
+ 
+ })();
     
-
-//let img = document.getElementById("laimagen");
-let canvas = document.getElementById('canvas');
-let dataURL = canvas.toDataURL();
-let png = document.getElementById("png");
-
-const getBase64 = () => {
-  //img.src = canvas.toDataURL("image/png");	
+ 
+ //let img = document.getElementById("laimagen");
+ let canvas = document.getElementById('canvas');
+ let dataURL = canvas.toDataURL();
+ let png = document.getElementById("png");
+ 
+ const getBase64 = () => {
+  //img.src = canvas.toDataURL("image/png");    
   console.log(dataURL);
   //debugBase64();
-};
-
-// const debugBase64 = (dataURL) => {
-//   let win = window.open();
-//   win.document.write('<iframe src="' + dataURL  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
-// };
-
-
-
-
-
-
-        // let picture = document.getElementById('snapshot');
-
-        // window.getPicture = () => {
-        //   let photoUser = picture.value;
-        //   console.log(photoUser);
-        // }
-
-
-// Initialize Cloud Firestore through Firebase
-// var db = firebase.firestore();
-
-// db.collection("users").add({
-//     first: "Ada",
-//     last: "Lovelace",
-//     born: 1815
-// })
-// .then(function(docRef) {
-//     console.log("Document written with ID: ", docRef.id);
-// })
-// .catch(function(error) {
-//     console.error("Error adding document: ", error);
-//});
+ };
