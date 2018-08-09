2eca25af2ac3e86aa54448155955e9102a867228
[Copelia] <iamcorallium@gmail.com>
2018-08-09 10:25:41 -0500

    Cambiando función para tomar foto y obteniendo data en base64

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 
   // Initialize Firebase
- 
  const config = {
     apiKey: "AIzaSyBcF7TEYv7lKctHHG6gyP6Abg_npVzMBNc",
     authDomain: "visitor-register-f8275.firebaseapp.com",
@@ -68,6 +67,101 @@ window.registerVisitor = () => {
     })
   
     };
+
+
+//Tomando foto    
+
+(function() {
+
+  var streaming = false,
+      video        = document.querySelector('#video'),
+      canvas       = document.querySelector('#canvas'),
+      photo        = document.querySelector('#photo'),
+      startbutton  = document.querySelector('#startbutton'),
+      width = 320,
+      height = 0;
+
+  navigator.getMedia = ( navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia ||
+                         navigator.msGetUserMedia);
+
+  navigator.getMedia(
+    {
+      video: true,
+      audio: false
+    },
+    function(stream) {
+      if (navigator.mozGetUserMedia) {
+        video.mozSrcObject = stream;
+      } else {
+        var vendorURL = window.URL || window.webkitURL;
+        video.srcObject = stream;
+      }
+      video.play();
+    },
+    function(err) {
+      console.log("An error occured! " + err);
+    }
+  );
+
+  video.addEventListener('canplay', function(ev){
+    if (!streaming) {
+      height = video.videoHeight / (video.videoWidth/width);
+      video.setAttribute('width', width);
+      video.setAttribute('height', height);
+      canvas.setAttribute('width', width);
+      canvas.setAttribute('height', height);
+      streaming = true;
+    }
+  }, false);
+
+  const takepicture = () => {
+    canvas.width = width;
+    canvas.height = height;
+    canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+    var data = canvas.toDataURL('image/png');
+    //photo.setAttribute('src', data);
+    //console.log(data);
+  }
+
+  startbutton.addEventListener('click', function(event){
+      takepicture();
+      getBase64();
+    event.preventDefault();
+  }, false);
+
+})();
+    
+
+//let img = document.getElementById("laimagen");
+let canvas = document.getElementById('canvas');
+let dataURL = canvas.toDataURL();
+let png = document.getElementById("png");
+
+const getBase64 = () => {
+  //img.src = canvas.toDataURL("image/png");	
+  console.log(dataURL);
+  //debugBase64();
+};
+
+// const debugBase64 = (dataURL) => {
+//   let win = window.open();
+//   win.document.write('<iframe src="' + dataURL  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
+// };
+
+
+
+
+
+
+        // let picture = document.getElementById('snapshot');
+
+        // window.getPicture = () => {
+        //   let photoUser = picture.value;
+        //   console.log(photoUser);
+        // }
+
 
 // Initialize Cloud Firestore through Firebase
 // var db = firebase.firestore();
