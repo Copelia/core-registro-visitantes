7a12cbb093b9b729c491e8ce82446930c9ff5a1f
[Copelia] <iamcorallium@gmail.com>
2018-08-10 17:23:28 -0500

    Resolviendo redirecciones

--- src/js/admin.js
+++ src/js/admin.js
@@ -58,7 +58,7 @@ const register = () => {
      firebase.auth().signOut()
      .then(function(){
         console.log("Saliendo...");
-        location.href = '../index.html';
+        location.href = '../views/home.html';
      })
      .catch(function (error){
        console.log(error);
--- src/js/main.js
+++ src/js/main.js
@@ -5,43 +5,42 @@ let db = firebase.firestore();
 
 //Registrando al visitante en la base de datos
 
-
   document.getElementById('register-visitor').addEventListener('click', (event) => {
+    let visitorName = document.getElementById("icon_prefix").value;
+    let issueVisitor = document.getElementById("icon_telephone").value;
+    let yearDateTime = firebase.firestore.FieldValue.serverTimestamp();
+
     //Hacer que este if funcione
-    // if (visitorName.value === ""  || issueVisitor.value === "") {
-    //   alert('No puedes continuar sin haber ingresado tus datos')
-    // } else {
-
-      let visitorName = document.getElementById("icon_prefix").value;
-      let issueVisitor = document.getElementById("icon_telephone").value;
-      let yearDateTime = firebase.firestore.FieldValue.serverTimestamp();
-
-      
+    if (visitorName === ""  || issueVisitor === "") {
+      alert('Para continuar, por favor ingresa los datos solicitados');
+    } else {
     db.collection('visitors').add({
       Visitante : visitorName,
       Asunto : issueVisitor,
       fechaYhora : yearDateTime
     }).then((docRef) => {
       console.log('Document written with ID: ', docRef.id);
-        location.href = 'src/select.html';
+        location.href = 'select.html';
     })
     .catch((error) => {
       console.error('Error adding document: ', error);
     });
-  //}
-   
+  
+  }
   
   });
 
+
+
 //Tomando foto    
 
-let player = document.getElementById('player'); 
+let player= document.getElementById('player'); 
 let snapshotCanvas = document.getElementById('snapshot');
 let captureButton = document.getElementById('capture');
 
   let handleSuccess = (stream) => {
     // Attach the video stream to the video element and autoplay.
-    player.srcObject = stream;
+    video.srcObject = stream;
   };
 
   captureButton.addEventListener('click', () => {
